2de185b3a3f03ac27e1f7fceb1e28ba32888a164
Ali Ashtiani <ajafaria@ucsd.edu>
2017-03-19 00:18:08 -0700

    change the view mode and map type

--- map.js
+++ map.js
@@ -1,9 +1,9 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: new google.maps.LatLng(2.8,-187.3),
-    mapTypeId: 'terrain'
+    zoom: 7,
+    center: new google.maps.LatLng(28.394857,84.124008),
+    mapTypeId: 'roadmap'
   });
 
 	var infowindow = new google.maps.InfoWindow();
@@ -13,7 +13,7 @@ function initMap() {
   var script = document.createElement('script');
   // This example uses a local copy of the GeoJSON stored at
   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  script.src = 'geo3.js';
+  script.src = 'geo2.js';
   document.getElementsByTagName('head')[0].appendChild(script);
 }
 
